21185e0200a5cd7572932b0516c1892b35446bed
Diogo Henrique <diogo.hglima@gmail.com>
2021-07-18 22:42:58 -0300

    converts project to typescript and functions

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -9,8 +9,14 @@ import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface InputProps {
+  name: string;
+  icon?: any;
+  placeholder: string;
+}
+
+const Input = ({ name, icon: Icon, ...rest }:InputProps): JSX.Element=> {
+  const inputRef = useRef<any>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
